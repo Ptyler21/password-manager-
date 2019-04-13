f94d6268541feb516ba4c60c111654ec1004271e
Ptyler21 <ptylerjr2@gmail.com>
2019-04-13 19:44:30 -0400

    created the SayHello function to ask the user for their username and password. The function returns a tuple. Then, created the function storeCred that takes the tuple from SayHello and will store it(?).

--- pwdmanagertest.py
+++ pwdmanagertest.py
@@ -1,16 +1,37 @@
+import argparse
 import sys
-
-class Login():
-    
+import getopt
 
 '''
-
-userLableInput = input("Please create a username: ")
-
-userPasswordInput = input("Please create a password: ")
+Asks the user for their name and password
+returns a tuple 
+python3 pwdmanagertest.py -u royos -p royos2
+('royos','royos2')
 '''
+def SayHello(username,password):
+
+   # userName = input("Please give me a username: ").strip()
+    #Password = input("Please give me a password: ").strip()
+   #this is tuple
+    return username , password
 
 '''
-
+stores the username and password from SayHello
 '''
+def storeCreds(*args):
+    for i in args:
+        print("printing out the arguments",i)
+    print(type(args))
 
+def main():
+
+   # SayHello(args.username,args.password)
+    parser = argparse.ArgumentParser(description='username and password for manager')
+    parser.add_argument('-u', '--username', type=str, help='The username for the account')
+    parser.add_argument('-p', '--password', type=str, help='The password for the account')
+    args = parser.parse_args()
+    print(SayHello(args.username, args.password))
+    storeCreds(SayHello(args.username,args.password))
+
+if __name__=="__main__":
+    main()
