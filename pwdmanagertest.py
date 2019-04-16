fad56067d6f3dccce9e9c3e4c6470c963c1de846
Ptyler21 <ptylerjr2@gmail.com>
2019-04-15 22:14:40 -0400

    Created functions that how the user enter their username and password from the command line, takes the hash of those values, and finally challenges the user by having them authenticate again. The userChallege function may be taken out in the future. It's here for a proof of concept.

--- pwdmanagertest.py
+++ pwdmanagertest.py
@@ -1,37 +1,60 @@
 import argparse
 import sys
 import getopt
+import pwd
+import crypt
+import getpass
+import keyring
+import hashlib
+import _md5
+from getpass import getpass
+import sys
 
-'''
-Asks the user for their name and password
-returns a tuple 
-python3 pwdmanagertest.py -u royos -p royos2
-('royos','royos2')
-'''
-def SayHello(username,password):
+parser = argparse.ArgumentParser(description='username and password for manager')
+parser.add_argument('-u', '--userId', type=str, help='The username for the account')
+parser.add_argument('-p', '--userPassword', type=str, help='The password for the account')
+args = parser.parse_args()
 
-   # userName = input("Please give me a username: ").strip()
-    #Password = input("Please give me a password: ").strip()
-   #this is tuple
-    return username , password
+def userLogin(userId , userPassword):
+    creds= {}
+    creds['user'] = userId.strip()
+    creds['cred'] = userPassword.strip()
+    return creds
 
-'''
-stores the username and password from SayHello
-'''
-def storeCreds(*args):
-    for i in args:
-        print("printing out the arguments",i)
-    print(type(args))
+def checkUser(**kwargs):
+    plainTextUser = kwargs['user']
+    hashedUserName = hashlib.md5(plainTextUser.encode('utf-8')).hexdigest()
+    #print("Original username hash", hashedUserName)
+    plainTextPassword = kwargs['cred']
+    hashedPassword = hashlib.md5(plainTextPassword.encode('utf-8')).hexdigest()
+    hashComparisonsDict= {}
+    hashComparisonsDict['idChallenge'] = hashedUserName
+    hashComparisonsDict['secretChallenge'] = hashedPassword
+    return hashComparisonsDict
+
+def userChallenge(**loginChallenge):
+    challengeUserNameInput = input("Please re-enter your username: ").strip()
+    challengeUserPassword = input("Please re-enter your password: ").strip()
+    if loginChallenge['idChallenge'] == hashlib.md5(challengeUserNameInput.encode('utf-8')).hexdigest():
+        print("The origional username hash " , loginChallenge['idChallenge'])
+        print("Your hash ", hashlib.md5(challengeUserNameInput.encode('utf-8')).hexdigest())
+        print("hash match!")
+    else:
+        print("No match try again")
+        sys.exit(0)
+    if loginChallenge['secretChallenge'] == hashlib.md5(challengeUserPassword.encode('utf-8')).hexdigest():
+        print("The origional Password hash " , loginChallenge['secretChallenge'])
+        print("Your password hash " , hashlib.md5(challengeUserPassword.encode('utf-8')).hexdigest())
+        print("Password hash match")
+
+    else:
+        print("No password match try again")
+        sys.exit(0)
+
 
 def main():
-
-   # SayHello(args.username,args.password)
-    parser = argparse.ArgumentParser(description='username and password for manager')
-    parser.add_argument('-u', '--username', type=str, help='The username for the account')
-    parser.add_argument('-p', '--password', type=str, help='The password for the account')
-    args = parser.parse_args()
-    print(SayHello(args.username, args.password))
-    storeCreds(SayHello(args.username,args.password))
-
+    userLogin(args.userId, args.userPassword)
+    checkUser(**userLogin(args.userId, args.userPassword))
+    userChallenge(**checkUser(**userLogin(args.userId, args.userPassword)))
 if __name__=="__main__":
     main()
