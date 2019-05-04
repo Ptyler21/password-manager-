6cbaa06da1922b1a5925f6febbc055308c7292ee
Ptyler21 <ptylerjr2@gmail.com>
2019-05-03 23:43:01 -0400

    using xlrd to read from my csv file.

--- pwdmanagertest.py
+++ pwdmanagertest.py
@@ -1,18 +1,25 @@
+from __future__ import  print_function
 import csv
 import codecs
+import join
+from os.path import join , dirname , abspath
+import xlrd
+from xlrd.sheet import ctype_text
 sitedict = {}
-fileReader = csv.reader(codecs.open('usercred.csv','r', 'utf-16'))
+#fileReader = csv.reader(codecs.open('usercred.csv','r', 'utf-16'))
 
-#maybe try a dictionary inside of a dictionary?
-for row in fileReader:
-    print(row[0])
-    sitedict["school"] = 'odu.edu'
-    if row[0] == "School ":
-        print("match found")
-    else:
-        print("match not found")
-    for site in sitedict.keys():
-        if row[0]== site:
-            print("matching keys found")
-        else:
-            print("no matching keys found")
+fileName = join(dirname(dirname(abspath(__file__))),
+                'password-manager-','usercred.xlsx')
+
+workBookOne = xlrd.open_workbook(fileName)
+
+workbookSheetNames = workBookOne.sheet_names()
+print('Sheet names: ', workbookSheetNames)
+
+categoryWorkBookOne = workBookOne.sheet_by_name(workbookSheetNames[0])
+
+categoryWorkBookOne = workBookOne.sheet_by_index(0)
+print('Sheet name: {}'.format(categoryWorkBookOne.name))
+
+#getting the first row
+rowOne = categoryWorkBookOne.row(0)
