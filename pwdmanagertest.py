17b829e5aa98161a9ba9a77663ffb9b77285ea87
Ptyler21 <ptylerjr2@gmail.com>
2019-05-07 19:22:17 -0400

    wrote categories site_name, user_name, and user_password to a csv files with credentials.

--- pwdmanagertest.py
+++ pwdmanagertest.py
@@ -1,25 +1,7 @@
-from __future__ import  print_function
 import csv
-import codecs
-import join
-from os.path import join , dirname , abspath
-import xlrd
-from xlrd.sheet import ctype_text
-sitedict = {}
-#fileReader = csv.reader(codecs.open('usercred.csv','r', 'utf-16'))
+with open("<path>",'w') as csvFile:
+    fieldnames = ['site_name', 'user_name', 'user_password']
+    fileWriter = csv.DictWriter(csvFile, fieldnames=fieldnames)
 
-fileName = join(dirname(dirname(abspath(__file__))),
-                'password-manager-','usercred.xlsx')
-
-workBookOne = xlrd.open_workbook(fileName)
-
-workbookSheetNames = workBookOne.sheet_names()
-print('Sheet names: ', workbookSheetNames)
-
-categoryWorkBookOne = workBookOne.sheet_by_name(workbookSheetNames[0])
-
-categoryWorkBookOne = workBookOne.sheet_by_index(0)
-print('Sheet name: {}'.format(categoryWorkBookOne.name))
-
-#getting the first row
-rowOne = categoryWorkBookOne.row(0)
+    fileWriter.writeheader()
+    fileWriter.writerow({'site_name': 'nsu.edu', 'user_name': 'royos', 'user_password': 'itssec'})
